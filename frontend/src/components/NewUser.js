038a2b56709003017927dc3819e41d6c77a7a301
malopezru <mateo.lopez@3dves.com>
2022-07-15 17:05:52 -0500

    update añadir usuarios y eliminar proyectos

--- frontend/src/components/NewUser.js
+++ frontend/src/components/NewUser.js
@@ -1,18 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './NewUser.css'
 
 function NewUser() {
     const [name, setName] = useState("");
     const [lastName, setLastName] = useState("");
-    const [username, setUserName] = useState("");
+    const [userName, setUserName] = useState("");
+    const [checkList, setCheckList] = useState([])
     const [projectsInfo, setProjectsInfo] = useState({ projects:[], response:[] });
+    var options = { };
+
+    const makeAPICall = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/api/users', {mode:'cors'});
+            const datos = await response.json();
+            setCheckList(datos.projects)
+        }catch (e) {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        makeAPICall();
+    }, [])
     
-    const handleSubmit = () => {
-        console.log(`${name}`)
-        console.log(`${lastName}`)
-        console.log(`${username}`)
-        console.log(`${projectsInfo}`)
-        alert("El Usuario ha sido ingresado, presione Volver para regresar a la pantalla de inicio")
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const first_name = `${name}`
+        const last_name = `${lastName}`
+        const username = `${userName}`
+
+        var checkboxes = document.getElementsByName('projects')
+        var selectedArray = 0;
+
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].checked) {
+                selectedArray += 1;
+            }
+        }
+
+        const projects = []
+
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].checked) {
+                projects.push(checkboxes[i].value);
+            }
+        }
+
+        
+        const data = {first_name, last_name, username, projects}
+        const datos = JSON.stringify(data)
+        console.log(datos)
+        
+        /* options = {
+            method: 'POST',
+            mode:'no-cors',
+            body: datos,
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        }
+        console.log(options) */
+            
+        fetch('http://localhost:3000/api/users', {
+            method: 'POST',
+            mode:'no-cors',
+            body: datos,
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+        
+        
+        if(first_name && lastName && userName && projects){
+            alert("El Usuario ha sido ingresado, presione Volver para regresar a la pantalla de inicio")
+        } else {
+            alert("Ingrese toda la información")
+        }
     }
 
     const handleChange = (event) => {
@@ -32,6 +94,7 @@ function NewUser() {
             });
           }
     }
+
     
     return (
         <div className='divisor'>
@@ -49,12 +112,17 @@ function NewUser() {
 
                 <label>
                     <p>Nombre de Usuario</p>
-                    <input type="text" name="username" onChange={(e) => setUserName(e.target.value)}/>
+                    <input type="text" name="userName" onChange={(e) => setUserName(e.target.value)}/>
                 </label>
 
                 <label>
                     <p>Proyectos a los que va a tener acceso:</p>
-                    <input type="checkbox" name="Proyecto1" value="Proyecto1" onChange={handleChange}/>Proyecto1
+                    {checkList.map((item, index) => (
+                        <div key={index}>
+                            <input value={checkList[index].project_name} name="projects" id={checkList[index].id} type="checkbox" onChange={handleChange}/>
+                            <span>{checkList[index].project_name}</span>
+                        </div>
+                        ))}
                     <hr/>
                     <textarea
                         className="form-control text"
